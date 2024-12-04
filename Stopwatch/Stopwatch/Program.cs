be588a857c50dbd747bb02e03697107b47fb45f7
ChickenPracticeCoding <trungkienhaui03@gmail.com>
2024-12-05 00:45:24 +0700

    stopwatch

---
+++ Stopwatch/Stopwatch/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Stopwatch
+{
+    internal class Program
+    {
+        public static void SelectionSort(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                // Hoán đổi giá trị
+                int temp = arr[minIndex];
+                arr[minIndex] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int[] arr = new int[100000];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = random.Next(1, 1000000); // Sinh số ngẫu nhiên
+            }
+
+            // Khởi tạo đối tượng Timer
+            watch timer = new watch();
+
+            // Bắt đầu đo thời gian
+            timer.Start();
+
+            // Gọi thuật toán sắp xếp chọn
+            SelectionSort(arr);
+
+            // Dừng đo thời gian
+            timer.Stop();
+
+            // In ra thời gian thực thi
+            Console.WriteLine("Thời gian thực thi thuật toán Selection Sort: " + timer.GetElapsedTime() + " ms");
+        }
+    }
+}
---
+++ Stopwatch/Stopwatch/watch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stopwatch
+{
+    internal class watch
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+
+        // Phương thức khởi tạo không tham số
+        public watch()
+        {
+            startTime = DateTime.Now; // Khởi tạo startTime với thời gian hiện tại
+        }
+
+        // Phương thức Start
+        public void Start()
+        {
+            startTime = DateTime.Now; // Đặt startTime lại là thời gian hiện tại
+        }
+
+        // Phương thức Stop
+        public void Stop()
+        {
+            endTime = DateTime.Now; // Đặt endTime là thời gian hiện tại
+        }
+
+        // Phương thức GetElapsedTime
+        public double GetElapsedTime()
+        {
+            return (endTime - startTime).TotalMilliseconds; // Trả về thời gian đã trôi qua (tính theo mili giây)
+        }
+    }
+}
